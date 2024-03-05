00abbc51278d6d52584c17023beb55048b7d5d5d
Cristina <cristinaaguilargda@gmail.com>
2024-03-05 13:07:24 +0100

    design change

--- Entrega6_rest-api/src/pods/character/character.styles.ts
+++ Entrega6_rest-api/src/pods/character/character.styles.ts
@@ -1,10 +1,5 @@
 import { css } from '@emotion/css';
 
-export const CharacterComponent = css`
-  display: flex;
-  justify-content: center;
-`;
-
 export const root = css`
   border: 3px solid #1976d2;
   border-radius: 10px;
